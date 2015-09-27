6cd41c9d41745e0491b65f246de3cb256ea2ab6d
Richmond Frimpong <richee305@yahoo.ca>
2015-09-27 17:15:26 -0400

    A WORKING WELCOME PAGE,NOTE EDGAR DONT PULL,SAVE YOUR WELCOME PAGE CODE SOMEWHERE FIRST

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,4 +1,3 @@
-
 /*
  * @author Richmond F, Edgar Z, Daniyal J
  * @cse : cse23004, cse23106, cse31034
@@ -9,158 +8,127 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
+import java.lang.reflect.InvocationTargetException;
 
-import javax.swing.Timer;
 import javax.swing.*;
 
-public class WelcomePage implements ActionListener {
-	private JFrame welcomeFrame;
-	private JFrame mainFrame;
+public class WelcomePage extends JFrame implements ActionListener {
+	/**
+	 * 
+	 */
+	private static JFrame welcomeFrame;
 	private String welcomeName;
 	private JPanel welcomePanel;
-	private JPanel welcomeButton;
+	private JButton welcomeButton;
 	private String value;
-	private String welcomeText;
+	private JProgressBar bar;
+	private static final long serialVersionUID = 1L;
 
+	static class welcomeThread extends Thread{
+		private static int DELAY = 100;
+		JProgressBar progressBar;
+		
+		public welcomeThread(JProgressBar bar){
+			progressBar = bar;
+		}
+		public void run(){
+			int max = progressBar.getMaximum();
+			int min = progressBar.getMinimum();
+			Runnable runner = new Runnable() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					int value  = progressBar.getValue();
+					progressBar.setValue(value + 1);	
+				}
+			};
+			for (int i = min ; i < max ; i++){
+				try{
+					EventQueue.invokeAndWait(runner);
+					//now we sleep
+					Thread.sleep(DELAY);
+					System.out.println("THREAD INTERRUPTED  " + Thread.interrupted());
+					System.out.println("CHECKER VALUE " + i);
+					if( i == max - 1){
+						System.out.println("RENDERING MAINPAGE . . . . . . . ");
+						MainPage display = new MainPage("PARKING KIOSK DEV ENV" , welcomeFrame); 
+						display.displayMainPage();
+						display.middlePostion();
+						
+						display.addTextFields();
+						display.addInputPanel();
+						display.addNumberButtons();
+						
+						display.addSubmitButtons("NEXT");
+						display.exitButton();
+						display.addClearButton();
+					}
+				}catch(InterruptedException ex1){
+					
+				}catch(InvocationTargetException ex2){
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	} //end of static class
 	public WelcomePage(JFrame frame, String text) {
-		this.welcomeFrame = frame;
+		welcomeFrame = frame;
 		this.welcomeName = text;
 	}
 
-	public void displayWelcomePanel() {
-
-		this.welcomeFrame = new JFrame(this.welcomeName);
-		this.welcomePanel = new JPanel();
-		this.welcomePanel.setLayout(new BoxLayout(this.welcomePanel, BoxLayout.Y_AXIS));
-		this.welcomeButton = new JPanel();
-		this.welcomeFrame.setSize(750, 500);
-		welcomeFrame.add(new welcomeClockPanel());
-		this.welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.welcomeFrame.setVisible(true);
-
+	public void displayWelcomeMessage() {
+		
 	}
-
+	
+	public void displayWelcomePanel() {
+		welcomeFrame = new JFrame(this.welcomeName);
+		welcomePanel = new JPanel();
+		//this.welcomePanel.setLayout(new BoxLayout(this.welcomePanel, BoxLayout.Y_AXIS));
+		welcomeFrame.setSize(700, 600);
+		welcomeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+	public void run(){
+		this.bar = new JProgressBar(0 , 50);
+		this.bar.setStringPainted(true);
+		this.welcomeButton = new JButton("CLICK");
+		this.welcomeButton.addActionListener(this);
+		welcomeFrame.add(bar , BorderLayout.NORTH);
+		welcomeFrame.add(welcomeButton , BorderLayout.SOUTH);
+		//this.welcomeFrame.add(welcomePanel);
+		welcomeFrame.setVisible(true);
+	}
 	public void middlePostion() {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		this.welcomeFrame.setLocation(dim.width / 2 - this.welcomeFrame.getSize().width / 2,
-				dim.height / 2 - this.welcomeFrame.getSize().height / 2);
-		this.welcomeFrame.setVisible(true);
-	}
-
-	public int welcomeButton() {
-		JButton continueButton = new JButton("Continue");
-		welcomeButton.add(continueButton);
-		this.welcomeFrame.add(welcomeButton, BorderLayout.SOUTH);
-		this.welcomeFrame.setVisible(false);
-		// this.mainFrame.setVisible(true);
-		continueButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				// DEBUG
-				System.out.println(e.getActionCommand() + "\n >>" + e.getSource());
-				System.out.println(e.paramString());
-				// DEBUG
-				if (e.getActionCommand().equals("Continue")) {
-					// System.exit(0);
-					MainPage mp = new MainPage("Parking Dev", mainFrame);
-					try {
-						mp.displayMainPage();
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					System.out.println("&&&&&&&&&&CONTINUE&&&&&&");
-				} else {
-					System.out.println("SOMETHING WEIRD HAPPEN !!!!!!!!!!!!");
-				}
-
-			}
-
-		});
-		this.welcomeFrame.setVisible(false);
-		return 0;
-	}
-
-	public void exitButton() {
-		JButton exitButton = new JButton("EXIT");
-		welcomeButton.add(exitButton);
-		this.welcomeFrame.add(welcomeButton, BorderLayout.SOUTH);
-		this.welcomeFrame.setVisible(true);
-		exitButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				// DEBUG
-				System.out.println(e.getActionCommand() + " ---- " + e.getSource());
-				System.out.println(e.paramString());
-				// DEBUG
-				if (e.getActionCommand().equals("EXIT")) {
-					System.out.println("****************");
-					System.exit(0);
-					System.out.println("****************");
-				} else {
-					System.out.println("SOMETHING WEIRD HAPPEN !!!!!!!!!!!!");
-				}
-
-			}
-
-		});
+		welcomeFrame.setLocation(dim.width / 2 - welcomeFrame.getSize().width / 2,
+				dim.height / 2 - welcomeFrame.getSize().height / 2);
+		welcomeFrame.setVisible(true);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent event) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		value = ((JButton) event.getSource()).getText().toLowerCase();
-		System.out.print("KEY => ");
-		System.out.println(value + " WAS PRESSED IN EMAILPAGE");
-	}
-
-	public class welcomeClockPanel extends JPanel {
-
-		private JLabel timer;
-		private JLabel welcomeLabel;
-
-		public welcomeClockPanel() {
-			setLayout(new BorderLayout());
-			timer = new JLabel();
-			welcomeLabel = new JLabel();
-			welcomeLabel.setVerticalAlignment(JLabel.CENTER);
-			welcomeLabel.setFont(UIManager.getFont("Label.font").deriveFont(Font.ROMAN_BASELINE, 16f));
-			timer.setVerticalAlignment(JLabel.BOTTOM);
-			timer.setHorizontalAlignment(JLabel.RIGHT);
-			timer.setFont(UIManager.getFont("Label.font").deriveFont(Font.ROMAN_BASELINE, 16f));
-			tickTock();
-			message();
-			add(timer);
-			add(welcomeLabel);
-			Timer clock = new Timer(500, new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					tickTock();
-					// message();
-				}
-			});
-			clock.setRepeats(true);
-			clock.setCoalesce(true);
-			clock.setInitialDelay(0);
-			clock.start();
-		}
-
-		public void message() {
-			welcomeLabel.setText("Welcome to York University's Parking System");
-		}
-
-		public void tickTock() {
-			timer.setText("Current Date/Time: " + DateFormat.getDateTimeInstance().format(new Date()));
+		this.welcomeButton.setEnabled(true);
+		Thread stepper = new welcomeThread(bar);
+		stepper.start();
+		if(stepper.isAlive()){
+			System.out.println("THREAD IS NOT DONE");
+			System.out.println(stepper.getId());
+			System.out.println(stepper.getPriority());
+			System.out.println(stepper.getState());
+			//System.out.println(stepper.activeCount());
+		}else{
+			System.out.println("FUCK!!");
 		}
 	}
+
 }
