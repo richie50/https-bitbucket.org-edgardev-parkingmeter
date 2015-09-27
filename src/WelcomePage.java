44788ac59a8d71af41404c154f4c0269327b0ab1
Edgar Zaganjori <edgarzaganjori@gmail.com>
2015-09-27 16:31:48 -0400

    Added timer + message, not working richie see if you can fix it mr JPanelExpert

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -9,35 +9,40 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 
+import javax.swing.Timer;
 import javax.swing.*;
 
 public class WelcomePage implements ActionListener {
 	private JFrame welcomeFrame;
+	private JFrame mainFrame;
 	private String welcomeName;
 	private JPanel welcomePanel;
 	private JPanel welcomeButton;
 	private String value;
+	private String welcomeText;
 
 	public WelcomePage(JFrame frame, String text) {
 		this.welcomeFrame = frame;
 		this.welcomeName = text;
 	}
 
-	public void displayWelcomeMessage() {
-		
-	}
-	
 	public void displayWelcomePanel() {
+
 		this.welcomeFrame = new JFrame(this.welcomeName);
 		this.welcomePanel = new JPanel();
 		this.welcomePanel.setLayout(new BoxLayout(this.welcomePanel, BoxLayout.Y_AXIS));
-        this.welcomeButton = new JPanel();
+		this.welcomeButton = new JPanel();
 		this.welcomeFrame.setSize(750, 500);
+		welcomeFrame.add(new welcomeClockPanel());
 		this.welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.welcomeFrame.setVisible(true);
 
@@ -51,12 +56,11 @@ public class WelcomePage implements ActionListener {
 	}
 
 	public int welcomeButton() {
-		MainPage mp = new MainPage("Parking Dev", welcomeFrame);
-		
 		JButton continueButton = new JButton("Continue");
 		welcomeButton.add(continueButton);
 		this.welcomeFrame.add(welcomeButton, BorderLayout.SOUTH);
-		this.welcomeFrame.setVisible(true);
+		this.welcomeFrame.setVisible(false);
+		// this.mainFrame.setVisible(true);
 		continueButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -67,6 +71,13 @@ public class WelcomePage implements ActionListener {
 				// DEBUG
 				if (e.getActionCommand().equals("Continue")) {
 					// System.exit(0);
+					MainPage mp = new MainPage("Parking Dev", mainFrame);
+					try {
+						mp.displayMainPage();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 					System.out.println("&&&&&&&&&&CONTINUE&&&&&&");
 				} else {
 					System.out.println("SOMETHING WEIRD HAPPEN !!!!!!!!!!!!");
@@ -75,6 +86,7 @@ public class WelcomePage implements ActionListener {
 			}
 
 		});
+		this.welcomeFrame.setVisible(false);
 		return 0;
 	}
 
@@ -110,5 +122,45 @@ public class WelcomePage implements ActionListener {
 		value = ((JButton) event.getSource()).getText().toLowerCase();
 		System.out.print("KEY => ");
 		System.out.println(value + " WAS PRESSED IN EMAILPAGE");
+	}
+
+	public class welcomeClockPanel extends JPanel {
+
+		private JLabel timer;
+		private JLabel welcomeLabel;
+
+		public welcomeClockPanel() {
+			setLayout(new BorderLayout());
+			timer = new JLabel();
+			welcomeLabel = new JLabel();
+			welcomeLabel.setVerticalAlignment(JLabel.CENTER);
+			welcomeLabel.setFont(UIManager.getFont("Label.font").deriveFont(Font.ROMAN_BASELINE, 16f));
+			timer.setVerticalAlignment(JLabel.BOTTOM);
+			timer.setHorizontalAlignment(JLabel.RIGHT);
+			timer.setFont(UIManager.getFont("Label.font").deriveFont(Font.ROMAN_BASELINE, 16f));
+			tickTock();
+			message();
+			add(timer);
+			add(welcomeLabel);
+			Timer clock = new Timer(500, new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					tickTock();
+					// message();
+				}
+			});
+			clock.setRepeats(true);
+			clock.setCoalesce(true);
+			clock.setInitialDelay(0);
+			clock.start();
+		}
+
+		public void message() {
+			welcomeLabel.setText("Welcome to York University's Parking System");
+		}
+
+		public void tickTock() {
+			timer.setText("Current Date/Time: " + DateFormat.getDateTimeInstance().format(new Date()));
+		}
 	}
 }
